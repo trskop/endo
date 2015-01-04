1786be949de5696bb138a0acada73ffde4a725d2
Peter Trško <peter.trsko@gmail.com>
2015-01-04 14:22:59 +0100

    Dual generic endomorphism folding
    In short, new interface, based on previous, introduces dualFoldEndo that
    applies endomorphism in reverse order. Classes FoldEndoArgs and AnEndo
    had to be extended with dual versions of their methods.

--- src/Data/Monoid/Endo/Fold.hs
+++ src/Data/Monoid/Endo/Fold.hs
@@ -22,6 +22,7 @@ module Data.Monoid.Endo.Fold
 
     -- * API
       foldEndo
+    , dualFoldEndo
     , FoldEndoArgs(..)
     , AnEndo(..)
 
@@ -37,7 +38,7 @@ import qualified Data.Foldable as Foldable (Foldable(foldMap))
 import Data.Function ((.), id)
 import Data.Functor.Identity (Identity(Identity))
 import Data.Maybe (Maybe(Just, Nothing))
-import Data.Monoid (Monoid(mempty, mconcat), Endo(Endo), (<>))
+import Data.Monoid (Dual(Dual), Endo(Endo), Monoid(mempty, mconcat), (<>))
 import Data.Typeable (Typeable)
 import GHC.Generics (Generic)
 import System.IO (IO)
@@ -66,11 +67,34 @@ import Control.Monad.Trans.Identity (IdentityT(..))
 foldEndo :: FoldEndoArgs args => args
 foldEndo = foldEndoArgs mempty
 
+-- | Same as 'foldEndo', but folds endomorphisms in reverse order.
+--
+-- Following are the same examples as for 'foldEndo' function. Please, note the
+-- differences in results.
+--
+-- Order in which endomorphisms are folded is reversed:
+--
+-- >>> dualFoldEndo (Endo (1:)) [(2:), (3:)] `appEndo` []
+-- [2,3,1]
+--
+-- For numbers it would look like:
+--
+-- >>> dualFoldEndo (Endo (+1)) [(+2), (*3)] `appEndo` 1
+-- 12
+--
+-- Above can be seen as:
+--
+-- >>> (*3) . (+2) . (+1) $ 1
+-- 12
+dualFoldEndo :: FoldEndoArgs args => args
+dualFoldEndo = dualFoldEndoArgs mempty
+
 -- {{{ FoldEndoArgs Type Class ------------------------------------------------
 
 class FoldEndoArgs a where
     type ResultOperatesOn a
-    foldEndoArgs :: Endo (ResultOperatesOn a) -> a
+    foldEndoArgs     ::       Endo (ResultOperatesOn a)  -> a
+    dualFoldEndoArgs :: Dual (Endo (ResultOperatesOn a)) -> a
 
 instance
     ( AnEndo a
@@ -79,31 +103,43 @@ instance
     ) => FoldEndoArgs (a -> r)
   where
     type ResultOperatesOn (a -> r) = ResultOperatesOn r
-    foldEndoArgs e e' = foldEndoArgs (e <> anEndo e')
+    foldEndoArgs     e e' = foldEndoArgs     (e <> anEndo    e')
+    dualFoldEndoArgs e e' = dualFoldEndoArgs (e <> aDualEndo e')
 
 instance FoldEndoArgs (Endo a) where
     type ResultOperatesOn (Endo a) = a
-    foldEndoArgs = id
+    foldEndoArgs              = id
+    dualFoldEndoArgs (Dual e) = e
+
+instance FoldEndoArgs (Dual (Endo a)) where
+    type ResultOperatesOn (Dual (Endo a)) = a
+    foldEndoArgs     = Dual
+    dualFoldEndoArgs = id
 
 instance (Applicative f, FoldEndoArgs r) => FoldEndoArgs (IdentityT f r) where
     type ResultOperatesOn (IdentityT f r) = ResultOperatesOn r
-    foldEndoArgs = IdentityT . pure . foldEndoArgs
+    foldEndoArgs     = IdentityT . pure . foldEndoArgs
+    dualFoldEndoArgs = IdentityT . pure . dualFoldEndoArgs
 
 instance FoldEndoArgs r => FoldEndoArgs (Either e r) where
     type ResultOperatesOn (Either e r) = ResultOperatesOn r
-    foldEndoArgs = Right . foldEndoArgs
+    foldEndoArgs     = Right . foldEndoArgs
+    dualFoldEndoArgs = Right . dualFoldEndoArgs
 
 instance FoldEndoArgs r => FoldEndoArgs (Identity r) where
     type ResultOperatesOn (Identity r) = ResultOperatesOn r
-    foldEndoArgs = Identity . foldEndoArgs
+    foldEndoArgs     = Identity . foldEndoArgs
+    dualFoldEndoArgs = Identity . dualFoldEndoArgs
 
 instance FoldEndoArgs r => FoldEndoArgs (IO r) where
     type ResultOperatesOn (IO r) = ResultOperatesOn r
-    foldEndoArgs = pure . foldEndoArgs
+    foldEndoArgs     = pure . foldEndoArgs
+    dualFoldEndoArgs = pure . dualFoldEndoArgs
 
 instance FoldEndoArgs r => FoldEndoArgs (Maybe r) where
     type ResultOperatesOn (Maybe r) = ResultOperatesOn r
-    foldEndoArgs = Just . foldEndoArgs
+    foldEndoArgs     = Just . foldEndoArgs
+    dualFoldEndoArgs = Just . dualFoldEndoArgs
 
 -- {{{ FoldEndoArgs Type Class ------------------------------------------------
 
@@ -111,18 +147,25 @@ instance FoldEndoArgs r => FoldEndoArgs (Maybe r) where
 
 class AnEndo a where
     type EndoOperatesOn a
-    anEndo :: a -> Endo (EndoOperatesOn a)
+
+    anEndo    :: a -> Endo (EndoOperatesOn a)
+
+    aDualEndo :: a -> Dual (Endo (EndoOperatesOn a))
+    aDualEndo = Dual . anEndo
 
 instance AnEndo (Endo a) where
     type EndoOperatesOn (Endo a) = a
-    anEndo = id
+    anEndo    = id
+    aDualEndo = Dual
 
 instance AnEndo (a -> a) where
     type EndoOperatesOn (a -> a) = a
-    anEndo = Endo
+    anEndo    = Endo
+    aDualEndo = Dual . Endo
 
 instance AnEndo a => AnEndo (Maybe a) where
     type EndoOperatesOn (Maybe a) = EndoOperatesOn a
+
     anEndo Nothing  = mempty
     anEndo (Just e) = anEndo e
 
@@ -133,11 +176,13 @@ newtype Foldable f a = Foldable {getFoldable :: f a}
 
 instance (Foldable.Foldable f, AnEndo a) => AnEndo (Foldable f a) where
     type EndoOperatesOn (Foldable f a) = EndoOperatesOn a
-    anEndo (Foldable fa) = Foldable.foldMap anEndo fa
+    anEndo    (Foldable fa) = Foldable.foldMap anEndo fa
+    aDualEndo (Foldable fa) = Foldable.foldMap aDualEndo fa
 
 instance AnEndo a => AnEndo [a] where
     type EndoOperatesOn [a] = EndoOperatesOn a
-    anEndo = anEndo . Foldable
+    anEndo    = anEndo    . Foldable
+    aDualEndo = aDualEndo . Foldable
 
 -- }}} Foldable Instances -----------------------------------------------------
 
@@ -150,7 +195,8 @@ instance
     ) => AnEndo (a, b)
   where
     type EndoOperatesOn (a, b) = EndoOperatesOn a
-    anEndo (a, b) = anEndo a <> anEndo b
+    anEndo    (a, b) = anEndo    a <> anEndo    b
+    aDualEndo (a, b) = aDualEndo a <> aDualEndo b
 
 instance
     ( AnEndo a
@@ -161,7 +207,8 @@ instance
     ) => AnEndo (a, b, c)
   where
     type EndoOperatesOn (a, b, c) = EndoOperatesOn a
-    anEndo (a, b, c) = anEndo a <> anEndo b <> anEndo c
+    anEndo    (a, b, c) = anEndo    a <> anEndo    b <> anEndo    c
+    aDualEndo (a, b, c) = aDualEndo a <> aDualEndo b <> aDualEndo c
 
 instance
     ( AnEndo a1
@@ -181,6 +228,13 @@ instance
         , anEndo a4
         ]
 
+    aDualEndo (a1, a2, a3, a4) = mconcat
+        [ aDualEndo a1
+        , aDualEndo a2
+        , aDualEndo a3
+        , aDualEndo a4
+        ]
+
 instance
     ( AnEndo a1
     , AnEndo a2
@@ -194,12 +248,21 @@ instance
     ) => AnEndo (a1, a2, a3, a4, a5)
   where
     type EndoOperatesOn (a1, a2, a3, a4, a5) = EndoOperatesOn a1
+
     anEndo (a1, a2, a3, a4, a5) = mconcat
         [ anEndo a1
         , anEndo a2
         , anEndo a3
         , anEndo a4
         , anEndo a5
+        ]
+
+    aDualEndo (a1, a2, a3, a4, a5) = mconcat
+        [ aDualEndo a1
+        , aDualEndo a2
+        , aDualEndo a3
+        , aDualEndo a4
+        , aDualEndo a5
         ]
 
 instance
@@ -217,6 +280,7 @@ instance
     ) => AnEndo (a1, a2, a3, a4, a5, a6)
   where
     type EndoOperatesOn (a1, a2, a3, a4, a5, a6) = EndoOperatesOn a1
+
     anEndo (a1, a2, a3, a4, a5, a6) = mconcat
         [ anEndo a1
         , anEndo a2
@@ -224,6 +288,15 @@ instance
         , anEndo a4
         , anEndo a5
         , anEndo a6
+        ]
+
+    aDualEndo (a1, a2, a3, a4, a5, a6) = mconcat
+        [ aDualEndo a1
+        , aDualEndo a2
+        , aDualEndo a3
+        , aDualEndo a4
+        , aDualEndo a5
+        , aDualEndo a6
         ]
 
 instance
@@ -243,6 +316,7 @@ instance
     ) => AnEndo (a1, a2, a3, a4, a5, a6, a7)
   where
     type EndoOperatesOn (a1, a2, a3, a4, a5, a6, a7) = EndoOperatesOn a1
+
     anEndo (a1, a2, a3, a4, a5, a6, a7) = mconcat
         [ anEndo a1
         , anEndo a2
@@ -251,6 +325,16 @@ instance
         , anEndo a5
         , anEndo a6
         , anEndo a7
+        ]
+
+    aDualEndo (a1, a2, a3, a4, a5, a6, a7) = mconcat
+        [ aDualEndo a1
+        , aDualEndo a2
+        , aDualEndo a3
+        , aDualEndo a4
+        , aDualEndo a5
+        , aDualEndo a6
+        , aDualEndo a7
         ]
 
 instance
@@ -272,6 +356,7 @@ instance
     ) => AnEndo (a1, a2, a3, a4, a5, a6, a7, a8)
   where
     type EndoOperatesOn (a1, a2, a3, a4, a5, a6, a7, a8) = EndoOperatesOn a1
+
     anEndo (a1, a2, a3, a4, a5, a6, a7, a8) = mconcat
         [ anEndo a1
         , anEndo a2
@@ -281,6 +366,17 @@ instance
         , anEndo a6
         , anEndo a7
         , anEndo a8
+        ]
+
+    aDualEndo (a1, a2, a3, a4, a5, a6, a7, a8) = mconcat
+        [ aDualEndo a1
+        , aDualEndo a2
+        , aDualEndo a3
+        , aDualEndo a4
+        , aDualEndo a5
+        , aDualEndo a6
+        , aDualEndo a7
+        , aDualEndo a8
         ]
 
 instance
@@ -304,6 +400,7 @@ instance
     ) => AnEndo (a1, a2, a3, a4, a5, a6, a7, a8, a9)
   where
     type EndoOperatesOn (a1, a2, a3, a4, a5, a6, a7, a8, a9) = EndoOperatesOn a1
+
     anEndo (a1, a2, a3, a4, a5, a6, a7, a8, a9) = mconcat
         [ anEndo a1
         , anEndo a2
@@ -314,6 +411,18 @@ instance
         , anEndo a7
         , anEndo a8
         , anEndo a9
+        ]
+
+    aDualEndo (a1, a2, a3, a4, a5, a6, a7, a8, a9) = mconcat
+        [ aDualEndo a1
+        , aDualEndo a2
+        , aDualEndo a3
+        , aDualEndo a4
+        , aDualEndo a5
+        , aDualEndo a6
+        , aDualEndo a7
+        , aDualEndo a8
+        , aDualEndo a9
         ]
 
 instance
@@ -339,6 +448,7 @@ instance
     ) => AnEndo (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)
   where
     type EndoOperatesOn (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) = EndoOperatesOn a1
+
     anEndo (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) = mconcat
         [ anEndo a1
         , anEndo a2
@@ -350,6 +460,19 @@ instance
         , anEndo a8
         , anEndo a9
         , anEndo a10
+        ]
+
+    aDualEndo (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) = mconcat
+        [ aDualEndo a1
+        , aDualEndo a2
+        , aDualEndo a3
+        , aDualEndo a4
+        , aDualEndo a5
+        , aDualEndo a6
+        , aDualEndo a7
+        , aDualEndo a8
+        , aDualEndo a9
+        , aDualEndo a10
         ]
 
 -- }}} Instances For Tuples ---------------------------------------------------
